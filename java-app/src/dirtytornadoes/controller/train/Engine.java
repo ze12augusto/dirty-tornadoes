b665ff1a75fbb66bb704b7a07dfe0ec561a0fffc
jeff <jeff@tjwallace.ca>
2009-04-16 00:51:08 +0000

    Need the hardware to test more I think

--- java-app/src/dirtytornadoes/controller/train/Engine.java
+++ java-app/src/dirtytornadoes/controller/train/Engine.java
@@ -1,5 +1,6 @@
 package dirtytornadoes.controller.train;
 
+import dirtytornadoes.controller.io.SerialIO;
 import dirtytornadoes.controller.train.events.TrainEvent;
 
 public class Engine extends TrainObject
@@ -18,36 +19,87 @@ public class Engine extends TrainObject
 		return inMotion;
 	}
 	
+	public void setInMotion()
+	{
+		inMotion = true;
+		brakesOn = false;
+	}
+	
+	public void setStopped()
+	{
+		inMotion = false;
+	}
+	
 	public void start() throws IllegalTrainOperation
 	{
 		if (brakesOn)
-			throw new IllegalTrainOperation("Cannot move train with brakes on");
+			throw new IllegalTrainOperation("Cannot start train with brakes on");
 		
 		inMotion = true;
 	}
 	
 	public void stop()
 	{
-		brakesOn = true;
+		brakesOn();
+		
+		try
+		{
+			Thread.sleep(1000);
+		}
+		catch (InterruptedException e)
+		{}
+		
 		inMotion = false;
 	}
 
-	public boolean isBrakesOn()
+	public boolean brakesAreOn()
 	{
 		return brakesOn;
+	}
+	
+	public void brakesOn()
+	{
+		brakesOn = true;
+	}
+	
+	public void brakesOff()
+	{
+		brakesOn = false;
 	}
 
 	@Override
 	public void handleTrainEvent( TrainEvent ev )
 	{
-		// TODO Auto-generated method stub
+		switch (ev.getType())
+		{
+			case TrainEvent.ENGINE_BRAKES_OFF:
+				brakesOff();
+			break;
+			
+			case TrainEvent.ENGINE_BRAKES_ON:
+				brakesOn();
+			break;
+			
+			case TrainEvent.ENGINE_IN_MOTION:
+				setInMotion();
+			break;
+			
+			case TrainEvent.ENGINE_STOPPED:
+				setStopped();
+			break;
+		}
 		
 	}
 
 	@Override
 	public void updateController()
 	{
-		// TODO Auto-generated method stub
+		// TODO send engine information
+		SerialIO s = SerialIO.getInstance();
+		
+		// send motion
+		
+		// send brakes
 		
 	}
 }
--- java-app/src/dirtytornadoes/controller/train/events/TrainEvent.java
+++ java-app/src/dirtytornadoes/controller/train/events/TrainEvent.java
@@ -6,20 +6,25 @@ public class TrainEvent extends EventObject
 {
 	private static final long serialVersionUID = 1L;
 	
+	// actions
+	public static final int ACTION_OPEN_DOORS = 00;
+	public static final int ACTION_CLOSE_DOORS = 01;
+	public static final int ACTION_LOCK_DOORS = 02;
+	public static final int ACTION_START_MOVING = 04;
+	public static final int ACTION_STOP_MOVING = 05;
+	public static final int ACTION_EMERGENCY = 06;
+	public static final int ACTION_EMERGENCY_RESET = 07;
+	
 	// door
-	public static final int DOOR_OPEN = 00;
-	public static final int DOOR_BLOCKED = 01;
-	public static final int DOOR_CLOSE = 02;
+	public static final int DOOR_OPEN = 10;
+	public static final int DOOR_BLOCK = 11;
+	public static final int DOOR_CLOSE = 12;
 	
 	// engine
-	public static final int ENGINE_BRAKES_ON = 10;
-	public static final int ENGINE_BRAKES_OFF = 11;
-	public static final int ENGINE_IN_MOTION = 12;
-	public static final int ENGINE_STOPPED = 13;
-	
-	// train
-	public static final int TRAIN_EMERGENCY = 21;
-	public static final int TRAIN_EMERGENCY_RESET = 22;
+	public static final int ENGINE_BRAKES_ON = 20;
+	public static final int ENGINE_BRAKES_OFF = 21;
+	public static final int ENGINE_IN_MOTION = 22;
+	public static final int ENGINE_STOPPED = 23;	
 	
 	private int type;
 	private String data;
